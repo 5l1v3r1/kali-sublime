683aa22d2efbdb25fea8ad190aa447e0df81d3f5
Michael Johnson <mygorillacode@gmail.com>
2017-04-10 19:42:17 +1200

    Simplified assert lib and added globals, colors and styles

--- lib/assert/assert_true.sh
+++ lib/assert/assert_true.sh
@@ -1,22 +1,3 @@
 assert_true() {
-	
-	local basefile=$(basename $0)
-	local baseline="$1"
-	local actual="$2"
-	local expected="true"
-	
-	if [ "${actual}" != "${expected}" ]; then
-		alert "[${RED}FAILED${CLEAR}] ${FUNCNAME[1]}"
-		alert
-		alert "  ${MAGENTA}File.....: ${basefile}, Line: ${baseline}${CLEAR}"
-		alert
-		alert "  ${CYAN}Expected.: ${expected}${CLEAR}"
-		alert "  ${RED}Actual...: ${actual}${CLEAR}"
-		alert
-		return 1
-	fi
-	
-	echo -e "[  ${GREEN}OK${CLEAR}  ] ${FUNCNAME[1]}" 
-	return 0
-	
+	expect_true $1 $2
 }
