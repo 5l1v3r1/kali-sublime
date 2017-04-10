4d3e31eb5fee18223322f6674c0f38c1904c6df9
Michael Johnson <mygorillacode@gmail.com>
2017-04-10 23:05:51 +1200

    Modified lib/import_all.sh to enable/disable messaging for diagnostics

--- lib/import_all.sh
+++ lib/import_all.sh
@@ -4,18 +4,19 @@ import_all() {
     local response="$2"
     local bash_files=$(find "${directory}" -name '*.sh' ! -name 'import_all.sh')
     local files_count=$(find "${directory}" -name '*.sh' ! -name 'import_all.sh' | wc -l)
+    local debug=false
 
-    # echo -e "\n\e[96m> Importing ${files_count} files from ${directory}\e[0m\n"
+    ! $debug || echo -e "\n\e[96m> Importing ${files_count} files from ${directory}\e[0m\n";
     for file in ${bash_files}
     do
         if [ -f "${file}" ]; then
-            # echo -e "[  \e[92mOK\e[0m  ] loaded ${file}"
+            ! $debug || echo -e "[  \e[92mOK\e[0m  ] loaded ${file}"
             source ${file}
-        # else
-            # echo -e "[\e91mFAILED\e[0m]] unable to load ${file}"
+        else
+            ! $debug || echo -e "[\e91mFAILED\e[0m]] unable to load ${file}"
         fi
     done
-    # echo -e "\n\e[92m\u2713 Successfully imported ${files_count} files from ${directory}\e[0m\n"
+    ! $debug || echo -e "\n\e[92m\u2713 Successfully imported ${files_count} files from ${directory}\e[0m\n"
 
 }
 
